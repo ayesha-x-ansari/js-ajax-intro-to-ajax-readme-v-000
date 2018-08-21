832403d218f2f5d417acfba45d861ff0b3245e7a
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-21 15:55:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,4 +24,3 @@ function showCommits() {
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("commits").innerHTML = commitsList
 }
-
