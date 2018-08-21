ba078580e538a3b70d4f0ba2c718b1f93a0b6953
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-21 15:53:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function showRepositories(event, data) {
   //this is set to the XMLHttpRequest object that fired the event
   console.log(this.responseText)
 }
- 
+
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories);
@@ -12,11 +12,8 @@ function getRepositories() {
 }
 
 function showRepositories(event, data) {
-  console.log(this.responseText)
-  let repoList = "<ul>"
-  for(var i=0;i < this.responseText.length; i++) {
-    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
-  }
-  repoList += "</ul>"
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
